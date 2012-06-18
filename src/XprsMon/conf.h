d4a4bf2cc674c821e2f5ac2325e3d1f1c54cd9ea
kalantari <kalantari>
2012-06-18 15:09:39 +0000

    added missing header files

---
+++ src/XprsMon/conf.h
@@ -0,0 +1,44 @@
+/*=========================< begin file & file header >=======================
+ *  References
+ *  
+ *    filename : conf.h
+ *    author   : JFG
+ *    company  : IOxOS
+ *    creation : may 30,2012
+ *    version  : 0.0.1
+ *
+ *----------------------------------------------------------------------------
+ *  Description
+ *
+ *    This file contain the declarations of all exported functions define in
+ *    pevulib.c
+ *
+ *----------------------------------------------------------------------------
+ *  Copyright Notice
+ *  
+ *    Copyright and all other rights in this document are reserved by 
+ *    IOxOS Technologies SA. This documents contains proprietary information    
+ *    and is supplied on express condition that it may not be disclosed, 
+ *    reproduced in whole or in part, or used for any other purpose other
+ *    than that for which it is supplies, without the written consent of  
+ *    IOxOS Technologies SA                                                        
+ *
+ *----------------------------------------------------------------------------
+ *  Change History
+ *  
+ * $Log: conf.h,v $
+ * Revision 1.1  2012/06/18 15:09:39  kalantari
+ * added missing header files
+ *
+ * Revision 1.1  2012/06/01 14:02:28  ioxos
+ * first checkin [JFG]
+ *
+ *
+ *=============================< end file header >============================*/
+
+#ifndef _H_CONF
+#define _H_CONF
+int xprs_conf_show( struct cli_cmd_para *);
+
+
+#endif /* _H_CONF */
---
+++ src/XprsMon/rdwr.h
@@ -0,0 +1,53 @@
+/*=========================< begin file & file header >=======================
+ *  References
+ *  
+ *    filename : rdwr.h
+ *    author   : JFG
+ *    company  : IOxOS
+ *    creation : may 30,2012
+ *    version  : 0.0.1
+ *
+ *----------------------------------------------------------------------------
+ *  Description
+ *
+ *    This file contain the declarations of all exported functions define in
+ *    pevulib.c
+ *
+ *----------------------------------------------------------------------------
+ *  Copyright Notice
+ *  
+ *    Copyright and all other rights in this document are reserved by 
+ *    IOxOS Technologies SA. This documents contains proprietary information    
+ *    and is supplied on express condition that it may not be disclosed, 
+ *    reproduced in whole or in part, or used for any other purpose other
+ *    than that for which it is supplies, without the written consent of  
+ *    IOxOS Technologies SA                                                        
+ *
+ *----------------------------------------------------------------------------
+ *  Change History
+ *  
+ * $Log: rdwr.h,v $
+ * Revision 1.1  2012/06/18 15:09:39  kalantari
+ * added missing header files
+ *
+ * Revision 1.1  2012/06/01 14:02:28  ioxos
+ * first checkin [JFG]
+ *
+ *
+ *=============================< end file header >============================*/
+
+#ifndef _H_RDWR
+#define _H_RDWR
+
+int xprs_rdwr_dma( struct cli_cmd_para *);
+int xprs_rdwr_evt( struct cli_cmd_para *);
+int xprs_rdwr_dm( struct cli_cmd_para *);
+int xprs_rdwr_fm( struct cli_cmd_para *);
+int xprs_rdwr_lm( struct cli_cmd_para *);
+int xprs_rdwr_pm( struct cli_cmd_para *);
+int xprs_rdwr_rmw( struct cli_cmd_para *);
+int xprs_rdwr_tm( struct cli_cmd_para *);
+int  rdwr_init( void);
+int  rdwr_exit( void);
+
+#endif /* _H_RDWR */
