c060d8a419ef216fb8edfb272a9978c12f5fc985
kalantari <kalantari>
2012-06-18 15:06:02 +0000

    XprsMon.h added

---
+++ src/XprsMon/XprsMon.h
@@ -0,0 +1,59 @@
+/*=========================< begin file & file header >=======================
+ *  References
+ *  
+ *    filename : .h
+ *    author   : JFG
+ *    company  : IOxOS
+ *    creation : may 30,2012
+ *    version  : 0.0.1
+ *
+ *----------------------------------------------------------------------------
+ *  Description
+ *
+ *    This file contain the declarations of all exported functions define in
+ *    pevulib.c
+ *
+ *----------------------------------------------------------------------------
+ *  Copyright Notice
+ *  
+ *    Copyright and all other rights in this document are reserved by 
+ *    IOxOS Technologies SA. This documents contains proprietary information    
+ *    and is supplied on express condition that it may not be disclosed, 
+ *    reproduced in whole or in part, or used for any other purpose other
+ *    than that for which it is supplies, without the written consent of  
+ *    IOxOS Technologies SA                                                        
+ *
+ *----------------------------------------------------------------------------
+ *  Change History
+ *  
+ * $Log: XprsMon.h,v $
+ * Revision 1.1  2012/06/18 15:06:02  kalantari
+ * XprsMon.h added
+ *
+ * Revision 1.1  2012/06/01 14:02:29  ioxos
+ * first checkin [JFG]
+ *
+ *
+ *=============================< end file header >============================*/
+
+#ifndef _H_XPRSMON
+#define _H_XPRSMON
+
+#include "eeprom.h"  
+#include "fpga.h"  
+#include "map.h"
+#include "rdwr.h"    
+#include "sflash.h"  
+#include "tst.h"
+#include "vme.h"
+#include "conf.h"   
+#include "fifo.h"    
+#include "i2c.h"   
+#ifdef MPC
+#include "mpc.h"  
+#endif
+#include "script.h"  
+#include "timer.h"   
+#include "tty.h"  
+
+#endif /* _H_XPRSMON */
