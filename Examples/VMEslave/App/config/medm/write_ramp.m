2b9b7fe3713c545d541d43eaf7741aecf8e5d27b
kalt_r <kalt_r>
2012-12-05 13:09:55 +0000

    minor bugfixes

--- Examples/VMEslave/App/config/medm/write_ramp.m
+++ Examples/VMEslave/App/config/medm/write_ramp.m
@@ -17,6 +17,7 @@ for k=2:32768
     ramp_inv(k)=ramp_inv(k-1)-1;
 end;
 
+%close all;
 figure(1);
 subplot(3,3,1);plot(ramp);title('Data in IFC1');
 subplot(3,3,2);plot(ramp);title('Data in IFC2');
@@ -29,7 +30,7 @@ caput('MTEST-VME-KR843:SMEM-WAVEFORM-W',ramp_inv);
 
 
 %Readback for verification
-caput('MTEST-VME-KR841:SMEM-ReadInitiator.PROC',1);
+%caput('MTEST-VME-KR841:SMEM-ReadInitiator.PROC',1);
 a= caget ('MTEST-VME-KR841:SMEM-DATA-R'); localread1=a.val;plot (localread1);
 caput('MTEST-VME-KR842:SMEM-ReadInitiator.PROC',1);
 a= caget ('MTEST-VME-KR842:SMEM-DATA-R'); localread2=a.val;plot (localread2);
@@ -42,7 +43,7 @@ subplot(3,3,6);plot(localread3);title('Readback Data from IFC3');
 
 
 %Readback using master and VME remote communication
-caput('MTEST-VME-KR841:SMEM-ReadInitiator.PROC',1);
+%caput('MTEST-VME-KR841:SMEM-ReadInitiator.PROC',1);
 a= caget ('MTEST-VME-KR841:SMEM-DATA-R'); remoteread1=a.val;plot (remoteread1);
 a= caget ('MTEST-VME-KR841:REMOTE2-DATA-R'); remoteread2=a.val;plot (remoteread2);
 a= caget ('MTEST-VME-KR841:REMOTE3-DATA-R'); remoteread3=a.val;plot (remoteread3);
@@ -50,3 +51,6 @@ a= caget ('MTEST-VME-KR841:REMOTE3-DATA-R'); remoteread3=a.val;plot (remoteread3
 subplot(3,3,7);plot(remoteread1);title('Readback Data from IFC1');
 subplot(3,3,8);plot(remoteread2);title('Remote Data from IFC2');
 subplot(3,3,9);plot(remoteread3);title('Remote Data from IFC3');
+
+figure(2)
+plot(remoteread3)
