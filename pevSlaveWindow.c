9123f5c3d4bb1b84a6b8402a02f4ae5672b7d1cd
zimoch <zimoch>
2015-06-03 12:42:35 +0000

    VME slave re-worked: remove unusable AM24 mode Allow smaller (1 instead of 16 MB) granularity for small mapping sizes removed buggy and unusable protocol parameter handling Allow to re-init slave windows (helps debugging) Better error and debug messages New function pevVmeShow

--- pevSlaveWindow.c
+++ pevSlaveWindow.c
@@ -13,16 +13,6 @@
 
 #include "pev.h"
 
-/* VME slave */
-typedef struct pevVmeSlaveMap {
-    unsigned int a32_base; 
-    unsigned int a32_size;
-    unsigned int a24_base; 
-    unsigned int a24_size;
-} pevVmeSlaveMap;
-
-static pevVmeSlaveMap glbVmeSlaveMap = {0, 0, 0, 0};	/* global slave map */
-
 /**
 **  VME slave window setup
 **  
@@ -31,73 +21,60 @@ static pevVmeSlaveMap glbVmeSlaveMap = {0, 0, 0, 0};	/* global slave map */
 
 int pevVmeSlaveMainConfig(const char* addrSpace, unsigned int mainBase, unsigned int mainSize)
 {
-  struct pev_ioctl_vme_conf vme_conf;
-  struct pev_ioctl_map_ctl vme_slv_map;
-  
-  if (!addrSpace)
-  {
-    printf("usage: pevVmeSlaveMainConfig (\"A24\"|\"A32\", base, size)\n");
-    return -1;
-  }
-  
-  if( !pev_init(0) )
-  {
-    printf("pevVmeSlaveMainConfig(): ERROR, Cannot allocate data structures to control IFC\n");
-    return -1;
-  }
-  pev_vme_conf_read(&vme_conf);
+    struct pev_ioctl_vme_conf vme_conf;
+    struct pev_ioctl_map_ctl vme_slv_map;
+    int status;
 
-  if (strcmp(addrSpace, "AM32") == 0)
-  { 
-    if (glbVmeSlaveMap.a32_size != 0)
+    if (!addrSpace)
     {
-      printf("pevVmeSlaveMainConfig(): ERROR, Main Config can be done only once, current a32_size = 0x%x a32_base = 0x%x \n", 
-      				glbVmeSlaveMap.a32_size, glbVmeSlaveMap.a32_base);
-      return -1;
+        printf("usage: pevVmeSlaveMainConfig (\"A24\"|\"A32\", base, size)\n");
+        return -1;
     }
+
+    pev_init(0);
+
+    pev_vme_conf_read(&vme_conf);
+
+    if (strcmp(addrSpace, "AM32") != 0)
+    {
+        printf("pevVmeSlaveMainConfig(): ERROR, can map to AM32 only\n");
+        return -1;
+    }
+
     vme_slv_map.sg_id = MAP_SLAVE_VME;	/* first clear all default winodws */
     pev_map_clear(&vme_slv_map);
+
     vme_conf.a32_size = mainSize;
     vme_conf.a32_base = mainBase;
-    glbVmeSlaveMap.a32_size = vme_conf.a32_size;
-    glbVmeSlaveMap.a32_base = vme_conf.a32_base;
-    if (mainSize == 0)			/* turn off slave window */
-    {
-      vme_conf.slv_ena = 0;
-      printf("\t => pevVmeSlaveMainConfig(): turnning OFF the whole vme slave window !!\n");
-    }
-    else 
-      vme_conf.slv_ena = 1; 
-  }
-  else
-  if (strcmp(addrSpace, "AM24") == 0)
-  { 
-    if (glbVmeSlaveMap.a24_size != 0)
-    {
-      printf("\t => pevVmeSlaveMainConfig(): ERROR, Main Config can be done only once, current \
-              a24_size = 0x%x a24_base = 0x%x \n", glbVmeSlaveMap.a24_size, glbVmeSlaveMap.a24_base);
-      return -1;
-    }
-    vme_conf.a24_size = mainSize;
-    vme_conf.a24_base = mainBase;
-    glbVmeSlaveMap.a24_size = vme_conf.a24_size;
-    glbVmeSlaveMap.a24_base = vme_conf.a24_base;
-  }
-  else
-  {
-    printf("\t => pevVmeSlaveMainConfig(): ERROR, invalid addrSpace (must be \"AM32\" or \"AM24\")\n");
-    return -1;
-  }
-  pev_vme_conf_write(&vme_conf);
-  pev_vme_conf_read(&vme_conf);
-  if (strcmp(addrSpace, "AM32") == 0 && vme_conf.slv_ena && (vme_conf.a32_base != mainBase || vme_conf.a32_size != mainSize))
-  {
-    printf("  pevVmeSlaveMainConfig(): == ERROR == mapping failed! \n\t current A32 slaveBase 0x%x [size 0x%x] \
-    		\n\t Address/Size Granularity must be 16MB=0x1000000\n", vme_conf.a32_base, vme_conf.a32_size);
-    return -1;
-  }
+    vme_conf.slv_ena = mainSize == 0 ? 0 : mainSize <= 0x8000000 ? VME_SLV_1MB | VME_SLV_ENA : VME_SLV_ENA;
 
-  return 0;
+    status = pev_vme_conf_write(&vme_conf);
+    pev_vme_conf_read(&vme_conf);
+
+    if (status)
+    {
+        printf("pevVmeSlaveMainConfig(): ERROR, pev_vme_conf_write() failed\n");
+    }
+
+    if (vme_conf.a32_size < mainSize)
+    {
+        printf("pevVmeSlaveMainConfig(): ERROR, slave map too large, got only %uMB instead of %uMB\n",
+            vme_conf.a32_size>>20, mainSize>>20);
+        return -1;
+    }
+    if (vme_conf.a32_size > mainSize)
+    {
+        printf("pevVmeSlaveMainConfig(): Info: slave map size increased to 0x%x=%uMB\n",
+            vme_conf.a32_size, vme_conf.a32_size>>20);
+    }
+    if (vme_conf.a32_base != mainBase)
+    {
+        printf("pevVmeSlaveMainConfig(): ERROR, slave map not adjusted. Move base from 0x%x to 0x%x\n",
+            mainBase, vme_conf.a32_base);
+        return -1;
+    }
+    return 0;
+
 }
 
 /**
@@ -114,71 +91,45 @@ int pevVmeSlaveMainConfig(const char* addrSpace, unsigned int mainBase, unsigned
 int pevVmeSlaveTargetConfig(const char* slaveAddrSpace, unsigned int winBase,  unsigned int winSize, 
 		const char* protocol, const char* target, unsigned int targetOffset, const char* swapping)
 {
+  struct pev_ioctl_vme_conf vme_conf;
+
   int mapMode = MAP_ENABLE | MAP_ENABLE_WR;
   if (!slaveAddrSpace || !protocol)
   {
     printf("usage: pevVmeSlaveTargetConfig (\"A24\"|\"A32\", base, size, \"BLT\"|\"MBLT\"|\"2eVME\"|\"2eSST160\"|\"2eSST233\"|\"2eSST320\", \"SH_MEM\"|\"PCIE\"|\"USR1/2\", offset, \"WS\"|\"DS\"|\"QS\")\n");
     return -1;
   }
-  
-  if (strcmp(slaveAddrSpace, "AM32") == 0)
-  { 
-    if (glbVmeSlaveMap.a32_size == 0)
-    {
-      printf("pevVmeSlaveTargetConfig(): ERROR, pevVmeSlaveMainConfig() has to be called first, Main \
-              a32_size = 0x%x \n", glbVmeSlaveMap.a32_size);
-      return -1;
-    }
-  }
-  else
-  if (strcmp(slaveAddrSpace, "AM24") == 0)
-  { 
-    if (glbVmeSlaveMap.a24_size == 0)
-    {
-      printf("pevVmeSlaveTargetConfig(): ERROR, pevVmeSlaveMainConfig() has to be called first, Main \
-              a24_size = 0x%x \n", glbVmeSlaveMap.a24_size);
-      return -1;
-    }
-  }
-  else
+
+  if (strcmp(slaveAddrSpace, "AM32") != 0)
   {
-    printf("pevVmeSlaveTargetConfig(): ERROR, invalid slaveAddrSpace \"%s\""
-        "(must be \"AM32\" or \"AM24\")\n",
-        slaveAddrSpace);
+    printf("pevVmeSlaveTargetConfig(): ERROR, can map to AM32 only\n");
     return -1;
   }
 
+  pev_init(0);
+  pev_vme_conf_read(&vme_conf);
+  if (!(vme_conf.slv_ena & VME_SLV_ENA))
+  {
+    printf("pevVmeSlaveTargetConfig(): ERROR, VME slave disabled, call pevVmeSlaveMainConfig first\n");
+    return -1;
+  }
+  
   if (swapping && *swapping)
   {
       if (strcmp(swapping, "AUTO") == 0)
         mapMode |= MAP_SWAP_AUTO;
       else
       {
-        printf("  pevVmeSlaveTargetConfig(): == ERROR == invalid swapping \"%s\""
+        printf("  pevVmeSlaveTargetConfig(): ERROR, invalid swapping \"%s\""
             "[valid options: AUTO]\n",
             swapping);
         return -1;
       }
   }
-  
 
   if (protocol && *protocol)
   {
-  
-      if(strcmp(protocol, "BLT")==0) 
-        mapMode |= DMA_VME_BLT;
-      else if(strcmp(protocol, "MBLT")==0) 
-        mapMode |= DMA_VME_MBLT;
-      else if(strcmp(protocol, "2eVME")==0) 
-        mapMode |= DMA_VME_2eVME;
-      else if(strcmp(protocol, "2eSST160")==0) 
-        mapMode |= DMA_VME_2e160;
-      else if(strcmp(protocol, "2eSST233")==0) 
-        mapMode |= DMA_VME_2e233;
-      else if(strcmp(protocol, "2eSST320")==0) 
-        mapMode |= DMA_VME_2e320;
-      else
-        mapMode|=strtol(protocol, NULL, 0);
+    printf("  pevVmeSlaveTargetConfig(): protocol parameter \"%s\" is useless and will be ignored\n", protocol);
   }
 
   if (strcmp(target, "SH_MEM") == 0)
@@ -196,20 +147,48 @@ int pevVmeSlaveTargetConfig(const char* slaveAddrSpace, unsigned int winBase,  u
         target);
     return -1;
   }
-
+  
   pevMapToAddr(0, MAP_SLAVE_VME, mapMode, targetOffset, winSize, winBase);
 
   return 0;
 }
 
-void pevVmeSlaveShow(int level)
+void pevVmeShow(void)
 {
-    printf("  Main VME slave window (A32) ");
-    if(glbVmeSlaveMap.a32_size == 0)
-        printf("turned OFF!\n");
-    else
-        printf("base=0x%08x size=0x%08x %dMB\n",
-            glbVmeSlaveMap.a32_base, glbVmeSlaveMap.a32_size, glbVmeSlaveMap.a32_size>>20);
+  struct pev_ioctl_vme_conf vme_conf;
+
+  pev_init(0);
+  pev_vme_conf_read(&vme_conf);
+  printf("current VME settings:\n"
+         " VME64x %s\n"
+         " VME master %s\n"
+         "  System Controller %s\n"
+         "  Sysreset %s\n"
+         "  Retry Timeout %d usec\n"
+         "  Arbitration mode %s\n"
+         "  Bus Timeout %d usec\n"
+         "  VME request mode %d\n"
+         "  VME request level %d\n"
+         " VME slave %s granularity=%uMB\n"
+         "  A32 slave base=0x%08x size=0x%x=%uMB\n"
+         "  A24 slave base=  0x%06x size=0x%x=%ukB\n"
+         "  Slave retry %s\n"
+         "  Slave burst %d\n",
+         vme_conf.x64 ? "enabled" : "disabled",
+         vme_conf.mas_ena ? "enabled" : "disabled",
+         vme_conf.slot1 ? "yes" : "no",
+         vme_conf.sysrst ? "enabled" : "disabled",
+         vme_conf.rto,
+         vme_conf.arb ? "yes" : "no",
+         vme_conf.bto,
+         vme_conf.req,
+         vme_conf.level,
+         vme_conf.slv_ena & VME_SLV_ENA ? "enabled" : "disabled",
+         vme_conf.slv_ena & VME_SLV_1MB ? 1 : 16,
+         vme_conf.a32_base, vme_conf.a32_size, vme_conf.a32_size>>20,
+         vme_conf.a24_base, vme_conf.a24_size, vme_conf.a24_size>>10,
+         vme_conf.slv_retry ? "no" : "yes",
+         vme_conf.burst);
 }
 
 static const iocshArg pevVmeSlaveMainConfigArg0 = { "addrSpace", iocshArgString };
@@ -259,10 +238,19 @@ static void pevVmeSlaveTargetConfigFunc (const iocshArgBuf *args)
     if (status != 0 && !interruptAccept) epicsExit(1);
 }
 
+static const iocshFuncDef pevVmeShowDef =
+    { "pevVmeShow", 0, NULL };
+    
+static void pevVmeShowFunc (const iocshArgBuf *args)
+{
+    pevVmeShow();
+}
+		
 static void pevSlaveWindowRegistrar ()
 {
     iocshRegister(&pevVmeSlaveMainConfigDef, pevVmeSlaveMainConfigFunc);
     iocshRegister(&pevVmeSlaveTargetConfigDef, pevVmeSlaveTargetConfigFunc);
+    iocshRegister(&pevVmeShowDef, pevVmeShowFunc);
 }
 
 epicsExportRegistrar(pevSlaveWindowRegistrar);
