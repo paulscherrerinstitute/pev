f5cc2c3093f672a55aa47b3a405aadd360f382f5
zimoch <zimoch>
2015-06-03 12:49:24 +0000

    Allow to map VME slave to RAM. Use DMA buffer for this. Improve debugging. Map indices start from 1 instead of 0 (used for command line tools). add pevMapPut function

--- pevMap.c
+++ pevMap.c
@@ -33,13 +33,15 @@ LOCAL struct pevMapEntry {
 
 LOCAL epicsMutexId pevMapListLock[MAX_PEV_CARDS];
 
-volatile void* pevMapExt(unsigned int card, unsigned int sg_id, unsigned int map_mode, size_t logicalAddress, size_t size, int flag, size_t localAddress)
+volatile void* pevMapExt(unsigned int card, unsigned int sg_id, unsigned int map_mode, size_t logicalAddress, size_t size, unsigned int flag, size_t localAddress)
 {
     struct pevMapEntry** pmapEntry;
     
     if (pevMapDebug)
-        printf("pevMap(card=%d, sg_id=0x%02x, mode=0x%02x, logicalAddress=0x%zx, size=0x%zx)\n",
-            card, sg_id, map_mode, logicalAddress, size);
+        printf("pevMap(card=%d, sg_id=0x%02x=%s, map_mode=0x%02x=%s%s%s%s, logicalAddress=0x%zx, size=0x%zx, flags=0x%x, localaddress=0x%zx)\n",
+            card, sg_id, pevSgName(sg_id),
+            map_mode, pevMapName(map_mode), map_mode & MAP_ENABLE ? "+ENABLE" :"", map_mode & MAP_ENABLE_WR? "+WRITABLE" : "", map_mode & MAP_SWAP_AUTO ? "+SWAP" : "",
+            logicalAddress, size, flag, localAddress);
 
     if (!pevMapListLock[card])
     {
@@ -64,7 +66,7 @@ volatile void* pevMapExt(unsigned int card, unsigned int sg_id, unsigned int map
         pevx_csr_wr(card, 0x80000404, mode);
     }
 
-    /* re-use existing maps as far as possible (is this necessary or does pev do so already?) */
+    /* re-use existing maps as far as possible */
     for (pmapEntry = &pevMapList[card]; *pmapEntry; pmapEntry = &(*pmapEntry)->next)
     {
         if ((*pmapEntry)->map.mode != map_mode) continue;
@@ -84,72 +86,107 @@ volatile void* pevMapExt(unsigned int card, unsigned int sg_id, unsigned int map
 
     if (!*pmapEntry)
     {
+        struct pev_ioctl_map_pg *pmap;
+
         if (pevMapDebug)
             printf("pevMap(): allocate new map\n");
         *pmapEntry = (struct pevMapEntry*) calloc(1, sizeof(struct pevMapEntry));
         if (*pmapEntry == NULL)
         {
-            errlogPrintf("pevMap: out of memory for new map structure\n");
+            errlogPrintf("pevMap(): out of memory for new map structure\n");
             epicsMutexUnlock(pevMapListLock[card]);
             return NULL;
         }
-        (*pmapEntry)->map.sg_id = sg_id;
-        (*pmapEntry)->map.mode = map_mode;
-        (*pmapEntry)->map.rem_addr = logicalAddress;
-        (*pmapEntry)->map.size = size;
-        (*pmapEntry)->map.loc_addr = localAddress;
-        (*pmapEntry)->map.flag = flag;
+        
+        pmap=&(*pmapEntry)->map;
+        pmap->sg_id = sg_id;
+        pmap->mode = map_mode;
+        pmap->rem_addr = logicalAddress;
+        pmap->size = size;
+        pmap->loc_addr = localAddress;
+        pmap->flag = flag;
+
+        
+        if (sg_id == MAP_SLAVE_VME && (map_mode & 0xf000) == MAP_SPACE_PCIE && logicalAddress == 0)  /* vme slave window to RAM */
+        {
+            if (pevMapDebug)
+                printf("pevMap(card=%d,... ): VME slave to RAM\n", card);
+            /* We need to allocate kernel RAM before we can map it. But for that we need the size first. */
+            pmap->mode &= ~MAP_ENABLE; /* disable until we have the RAM */
+        }
 
         /* pevx_map_alloc rounds boundaries as necessary, results in rem_base and win_size */
-        pevx_map_alloc(card, &(*pmapEntry)->map);
-        if ((*pmapEntry)->map.win_size < (*pmapEntry)->map.size)
+        pevx_map_alloc(card, pmap);
+        if (pmap->win_size < size)
         {
-            errlogPrintf("pevMap(card=%d, sg_id=0x%02x, mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): allocating map failed\n",
-                card, sg_id, map_mode, logicalAddress, size);
-            free(*pmapEntry);
-            *pmapEntry = NULL;
-            epicsMutexUnlock(pevMapListLock[card]);
-            return NULL;
+            errlogPrintf("pevMap(card=%d, sg_id=0x%02x=%s, map_mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): allocating map failed. Got size=0x%zx\n",
+                card, sg_id, pevSgName(sg_id), map_mode, logicalAddress, size, pmap->win_size);
+            goto fail;
         }
 
         if (pevMapDebug)
-            printf("pevMap(card=%d, sg_id=0x%02x, mode=0x%02x, ...): wanted 0x%lx - 0x%lx, got 0x%lx - 0x%lx @0x%lx\n",
-                card, (*pmapEntry)->map.sg_id, (*pmapEntry)->map.mode,
-                (*pmapEntry)->map.rem_addr, (*pmapEntry)->map.rem_addr + (*pmapEntry)->map.size,
-                (*pmapEntry)->map.rem_base, (*pmapEntry)->map.rem_base + (*pmapEntry)->map.win_size,
-                (*pmapEntry)->map.loc_addr);
+            printf("pevMap(card=%d, sg_id=0x%02x=%s, map_mode=0x%02x, ...): wanted 0x%lx - 0x%lx, got 0x%lx - 0x%lx @0x%lx\n",
+                card, sg_id, pevSgName(sg_id), map_mode,
+                pmap->rem_addr, pmap->rem_addr + pmap->size,
+                pmap->rem_base, pmap->rem_base + pmap->win_size,
+                pmap->loc_addr);
         
         /* loc_addr may point to an address not suitable for mmap.
-         * It points to the address that maps to rem_addr, but we need rem_base for mmap.
+         * It points to the address that maps to rem_addr, but we need loc_base for mmap.
          * Also extend size to win_size. That will map the whole (extended) window.
          * Reconstruct the user requested address later from distance rem_base to logicalAddress.
          */
-        (*pmapEntry)->map.loc_addr -= (*pmapEntry)->map.rem_addr - (*pmapEntry)->map.rem_base;
-        (*pmapEntry)->map.size = (*pmapEntry)->map.win_size;
+        pmap->loc_addr -= pmap->rem_addr - pmap->rem_base;
+        pmap->size = pmap->win_size;
 
-        if (sg_id != MAP_SLAVE_VME) /* no mmap for VME slave windows */
+        if (sg_id != MAP_SLAVE_VME)  /* mmap master windows */
         {
-            if (pevx_mmap(card, &(*pmapEntry)->map) == MAP_FAILED)
+            if (pevx_mmap(card, pmap) == MAP_FAILED)
             {
-                errlogPrintf("pevMap(card=%d, sg_id=0x%02x, mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): mapping to user space failed: %s\n",
-                    card, sg_id, map_mode, logicalAddress, size, strerror(errno));
-                pevx_map_free(card, &(*pmapEntry)->map);
-                free(*pmapEntry);
-                *pmapEntry = NULL;
-                epicsMutexUnlock(pevMapListLock[card]);
-                return NULL;
+                errlogPrintf("pevMap(card=%d, sg_id=0x%02x=%s, map_mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): mapping to user space failed: %s\n",
+                    card, sg_id, pevSgName(sg_id), map_mode, logicalAddress, size, strerror(errno));
+                goto fail;
             }
+        }
+        if (sg_id == MAP_SLAVE_VME && (map_mode & 0xf000) == MAP_SPACE_PCIE && logicalAddress == 0)  /* vme slave window to RAM */
+        {
+            /* allocate RAM for slave window */
+            if (pevMapDebug)
+                printf("pevMap(card=%d,... ): allocate RAM\n", card);
+            
+            pmap->usr_addr = pevDmaRealloc(card, NULL, pmap->win_size);
+            
+            if (pmap->usr_addr == NULL)
+            {
+                errlogPrintf("pevMap(card=%d, sg_id=0x%02x=%s, map_mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): pevDmaRealloc() failed: %s\n",
+                    card, sg_id, pevSgName(sg_id), map_mode, logicalAddress, size, strerror(errno));
+                goto fail;
+            }
+            if (pevMapDebug)
+                printf("pevMap(card=%d, sg_id=0x%02x=%s, map_mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): buffer=%p\n",
+                    card, sg_id, pevSgName(sg_id), map_mode, logicalAddress, size, pmap->usr_addr);  
+
+            /* now re-map to RAM buffer */
+            pmap->mode = map_mode;
+            pmap->rem_addr = pevDmaUsrToBusAddr(card, pmap->usr_addr);
+            pevx_map_modify(card, pmap);
         }
     }
 
     (*pmapEntry)->refcount++;
     epicsMutexUnlock(pevMapListLock[card]);
     if (pevMapDebug)
-        printf("pevMap(card=%d, sg_id=0x%02x, mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): rem_base=0x%lx, usr_addr=%p\n",
-            card, sg_id, map_mode, logicalAddress, size, (*pmapEntry)->map.rem_base, (*pmapEntry)->map.usr_addr);
+        printf("pevMap(card=%d, sg_id=0x%02x=%s, map_mode=0x%02x, logicalAddress=0x%zx, size=0x%zx): rem_base=0x%lx, usr_addr=%p\n",
+            card, sg_id, pevSgName(sg_id), map_mode, logicalAddress, size, (*pmapEntry)->map.rem_base, (*pmapEntry)->map.usr_addr);
 
     /* usr_addr mapps to rem_base, user logicalAddress needs offset into this block */
     return logicalAddress - (*pmapEntry)->map.rem_base + (char*)(*pmapEntry)->map.usr_addr;
+fail:
+    if ((*pmapEntry)->map.win_size != 0)
+        pevx_map_free(card, &(*pmapEntry)->map);
+    free(*pmapEntry);
+    epicsMutexUnlock(pevMapListLock[card]);
+    return NULL;
 }
 
 void pevUnmap(void* ptr)
@@ -168,10 +205,11 @@ void pevUnmap(void* ptr)
                 if (--mapEntry->refcount == 0)
                 {
                     if (pevMapDebug)
-                        printf("pevUnmap(): releasing memory map card %d %s base=0x%08lx size=0x%x %dMB\n",
-                            card,
-                            pevMapName(mapEntry->map.mode),
-                            mapEntry->map.rem_base,mapEntry->map.win_size, mapEntry->map.win_size>>20);
+                        printf("pevUnmap(): releasing memory map card %d %s base=0x%08lx size=0x%x=%uMB\n",
+                            card, pevMapName(mapEntry->map.mode),
+                            mapEntry->map.rem_base, mapEntry->map.win_size, mapEntry->map.win_size>>20);
+                    if (mapEntry->map.usr_addr != MAP_FAILED && mapEntry->map.usr_addr != NULL)
+                        pevx_munmap(card, &mapEntry->map);
                     pevx_map_free(card, &mapEntry->map);
                     epicsMutexUnlock(pevMapListLock[card]);
                     return;
@@ -186,10 +224,10 @@ const char* pevSgName(unsigned int sg_id)
 {
     switch (sg_id)
     {
-        case MAP_PCIE_MEM:  return "PCIe MEM";
-        case MAP_PCIE_PMEM: return "PCIe PMEM";
+        case MAP_PCIE_MEM:  return "MEM";
+        case MAP_PCIE_PMEM: return "PMEM";
         case MAP_PCIE_CSR:  return "PCIe CSR";
-        case MAP_VME_SLAVE: return "VME SLAVE";
+        case MAP_VME_SLAVE: return "VME A32";
         case MAP_VME_ELB:   return "VME ELB";
         default:            return "????????";
     }
@@ -221,40 +259,15 @@ const char* pevMapName(unsigned int map_mode)
     }
 }
 
-void pevMapShow(int vmeOnly)
+size_t pevMapPageSize(unsigned int card, unsigned int sg_id)
 {
-    struct pevMapEntry* mapEntry;
-    unsigned int card;
-    unsigned int index;
-    int anything_reported = 0;
-    
-    for (card = 0; card < MAX_PEV_CARDS; card++)
-    {
-        if (pevMapList[card])
-        {
-            printf("  card %d: (VME maps use %s mode)\n",
-                card, pevx_csr_rd(card, 0x80000404) & (1<<5) ?
-                    "supervisory" : "user");
-            for (mapEntry = pevMapList[card], index = 0; mapEntry; mapEntry = mapEntry->next, index++)
-            {
-                if (vmeOnly && (mapEntry->map.mode & MAP_SPACE_MASK) != MAP_SPACE_VME) continue;
-                printf("   %d: %-9s -> %-8s base=0x%08lx size=0x%08x %dMB %s %s\n",
-                    index,
-                    pevSgName(mapEntry->map.sg_id),
-                    pevMapName(mapEntry->map.mode), 
-                    mapEntry->map.rem_base,
-                    mapEntry->map.win_size,
-                    mapEntry->map.win_size>>20,
-                    mapEntry->map.mode & MAP_ENABLE ? mapEntry->map.mode & MAP_ENABLE_WR ? "R/W" : "R" : "disabled",
-                    mapEntry->map.mode & MAP_SWAP_AUTO ? "auto swap" : "");
-            }
-            anything_reported = 1;
-        }
-    }
-    if (!anything_reported)
-    {
-        printf("  No map installed\n");
-    }
+    struct pev_ioctl_map_ctl map_ctl;
+
+    map_ctl.sg_id = sg_id;
+    map_ctl.map_p = NULL;
+    map_ctl.pg_size = 0;
+    pevx_map_read(card, &map_ctl);
+    return map_ctl.pg_size;
 }
 
 int pevMapGetMapInfo(const void* address, struct pevMapInfo* info)
@@ -280,6 +293,77 @@ int pevMapGetMapInfo(const void* address, struct pevMapInfo* info)
     return 0;
 }
 
+void pevMapShow(int vmeOnly)
+{
+    struct pevMapEntry* mapEntry;
+    unsigned int card;
+    unsigned int index;
+    int anything_reported = 0;
+    
+    for (card = 0; card < MAX_PEV_CARDS; card++)
+    {
+        struct pev_ioctl_vme_conf vme_conf;
+        pevx_init(card);
+        if (pevx_vme_conf_read(card, &vme_conf) != 0) continue;
+        if (pevMapList[card] || (pevx_vme_conf_read(card, &vme_conf) == 0 && (vme_conf.slv_ena & VME_SLV_ENA)))
+        {
+            int supervisory = pevx_csr_rd(card, 0x80000404) & (1<<5);
+            if (vme_conf.slv_ena & VME_SLV_ENA)
+            {
+                printf("      %-8s 0x%08x -> %-8s               size=0x%08x=%uMB\n",
+                    "VME A32", vme_conf.a32_base, "VME SLAVE",
+                    vme_conf.a32_size, vme_conf.a32_size>>20);
+            }
+            for (mapEntry = pevMapList[card], index = 1; mapEntry; mapEntry = mapEntry->next, index++)
+            {
+                struct pev_ioctl_map_pg *pmap = &mapEntry->map;
+                struct pev_ioctl_map_ctl map_ctl;
+                int space = pmap->mode & MAP_SPACE_MASK;
+                
+                if (vmeOnly && space != MAP_SPACE_VME && pmap->sg_id != MAP_VME_SLAVE) continue;
+
+                map_ctl.sg_id = pmap->sg_id;
+                map_ctl.map_p = NULL;
+                map_ctl.pg_size = 0;
+                pevx_map_read(card, &map_ctl);
+
+                printf("   %d: %-8s 0x%08lx -> %-8s%3s 0x%08lx  size=0x%08x=%uMB %s %s\n",
+                    index,
+                    pevSgName(pmap->sg_id),
+                    pmap->sg_id == MAP_PCIE_MEM || pmap->sg_id == MAP_PCIE_PMEM ? (size_t)pmap->usr_addr :
+                    pmap->loc_addr + (pmap->sg_id == MAP_SLAVE_VME ? vme_conf.a32_base : 0),
+                    space == MAP_SPACE_PCIE && pmap->usr_addr ? "RAM" : pevMapName(pmap->mode),
+                    space == MAP_SPACE_VME ? supervisory ? "SUP" : "USR" : "",
+                    space == MAP_SPACE_PCIE && pmap->usr_addr ? (size_t)pmap->usr_addr : pmap->rem_base,
+                    pmap->win_size, pmap->win_size>>20,
+                    (pmap->mode & MAP_ENABLE) ? (pmap->mode & MAP_ENABLE_WR) ? "R/W" : "R" : "disabled",
+                    (pmap->mode & MAP_SWAP_AUTO) ? "auto-swap" : "");
+            }
+            anything_reported = 1;
+        }
+    }
+    if (!anything_reported)
+    {
+        printf("  No map installed\n");
+    }
+}
+
+static const iocshFuncDef pevMapShowDef =
+    { "pevMapShow", 0, NULL };
+
+static void pevMapShowFunc (const iocshArgBuf *args)
+{
+    pevMapShow(0);
+}
+
+static const iocshFuncDef vmeMapShowDef =
+    { "vmeMapShow", 0, NULL };
+
+static void vmeMapShowFunc (const iocshArgBuf *args)
+{
+    pevMapShow(1);
+}
+
 void pevMapDisplay(unsigned int card, unsigned int map, size_t start, unsigned int dlen, size_t bytes)
 {
     struct pevMapEntry* mapEntry;
@@ -299,14 +383,14 @@ void pevMapDisplay(unsigned int card, unsigned int map, size_t start, unsigned i
         printf("invalid card number %d\n", card);
         return;
     }
-    index = 0;
+    index = 1;
     for (mapEntry = pevMapList[card]; mapEntry; mapEntry = mapEntry->next)
     {
         if (index++ == map) break;
     }
     if (!mapEntry)
     {
-        printf("invalid index number %d\n", map);
+        printf("invalid map number %d\n", map);
         return;
     }
     if (card != save_card || map != save_map)
@@ -347,7 +431,7 @@ void pevMapDisplay(unsigned int card, unsigned int map, size_t start, unsigned i
     printf ("%s base=0x%08lx @%p\n", pevMapName(mapEntry->map.mode), mapEntry->map.rem_base, mapEntry->map.usr_addr);
     if (mapEntry->map.usr_addr == NULL)
     {
-        printf ("Not mapped to user space\n");
+        printf ("not mapped to user space\n");
         return;
     }
     for (i = 0; i < bytes; i += 16)
@@ -435,20 +519,73 @@ static void pevMapDisplayFunc (const iocshArgBuf *args)
         args[0].ival, args[1].ival, args[2].ival, args[3].ival, args[4].ival);
 }
 
-static const iocshFuncDef pevMapShowDef =
-    { "pevMapShow", 0, NULL };
-
-static void pevMapShowFunc (const iocshArgBuf *args)
+void pevMapPut(unsigned int card, unsigned int map, size_t offset, unsigned int dlen, int value)
 {
-    pevMapShow(0);
+    struct pevMapEntry* mapEntry;
+    unsigned int index;
+
+    if (card > MAX_PEV_CARDS || !pevMapList[card])
+    {
+        printf("invalid card number %d\n", card);
+        return;
+    }
+    index = 0;
+    for (mapEntry = pevMapList[card]; mapEntry; mapEntry = mapEntry->next)
+    {
+        if (index++ == map) break;
+    }
+    if (!mapEntry)
+    {
+        printf("invalid map number %d\n", map);
+        return;
+    }
+    if (offset+dlen >= mapEntry->map.win_size)
+    {
+        errlogPrintf("address 0x%x out of range\n", offset);
+        return;
+    }
+    if (mapEntry->map.usr_addr == NULL)
+    {
+        printf ("not mapped to user space\n");
+        return;
+    }
+    switch (dlen)
+    {
+        case 1:
+            *(epicsUInt8*)((char*)mapEntry->map.usr_addr + offset) = (epicsUInt8)value;
+            break;
+        case 2:
+            *(epicsUInt16*)((char*)mapEntry->map.usr_addr + offset) = (epicsUInt16)value;
+            break;
+        case 4:
+            *(epicsUInt32*)((char*)mapEntry->map.usr_addr + offset) = (epicsUInt32)value;
+            break;
+        default:
+            errlogPrintf("illegal dlen %d, must be 1, 2, or 4\n", dlen);
+            return;
+    }
 }
 
-static const iocshFuncDef vmeMapShowDef =
-    { "vmeMapShow", 0, NULL };
+static const iocshArg pevMapPutArg0 = { "card", iocshArgInt };
+static const iocshArg pevMapPutArg1 = { "map", iocshArgInt };
+static const iocshArg pevMapPutArg2 = { "offset", iocshArgInt };
+static const iocshArg pevMapPutArg3 = { "dlen", iocshArgInt };
+static const iocshArg pevMapPutArg4 = { "value", iocshArgInt };
+static const iocshArg * const pevMapPutArgs[] = {
+    &pevMapPutArg0,
+    &pevMapPutArg1,
+    &pevMapPutArg2,
+    &pevMapPutArg3,
+    &pevMapPutArg4,
+};
 
-static void vmeMapShowFunc (const iocshArgBuf *args)
+static const iocshFuncDef pevMapPutDef =
+    { "pevMapPut", 5, pevMapPutArgs };
+    
+static void pevMapPutFunc (const iocshArgBuf *args)
 {
-    pevMapShow(1);
+    pevMapPut(
+        args[0].ival, args[1].ival, args[2].ival, args[3].ival, args[4].ival);
 }
 
 LOCAL void pevMapExit(void* dummy)
@@ -462,7 +599,7 @@ LOCAL void pevMapExit(void* dummy)
         for (mapEntry = pevMapList[card]; mapEntry; mapEntry = mapEntry->next)
         {
             if (pevMapDebug)
-                printf("pevMapExit(): releasing memory map card %d %s base=0x%08lx size=0x%x %dMB\n",
+                printf("pevMapExit(): releasing memory map card %d %s base=0x%08lx size=0x%x=%uMB\n",
                     card,
                     pevMapName(mapEntry->map.mode),
                     mapEntry->map.rem_base,mapEntry->map.win_size, mapEntry->map.win_size>>20);
@@ -472,9 +609,11 @@ LOCAL void pevMapExit(void* dummy)
                 pevx_map_free(card, &mapEntry->map);
         }
     }
+    if (pevMapDebug)
+        printf("pevMapExit(): done\n");
 }
 
-int pevMapInit()
+int pevMapInit(void)
 {
     unsigned int card;
 
@@ -495,6 +634,7 @@ int pevMapInit()
     iocshRegister(&pevMapShowDef, pevMapShowFunc);
     iocshRegister(&vmeMapShowDef, vmeMapShowFunc);
     iocshRegister(&pevMapDisplayDef, pevMapDisplayFunc);
+    iocshRegister(&pevMapPutDef, pevMapPutFunc);
     
     return S_dev_success;
 }
