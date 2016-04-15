0870b536b62ce9ae5d65cd5837cf46ab9d938adf
Dirk Zimoch <dirk.zimoch@psi.ch>
2016-04-15 16:20:16 +0200

    bugfix: crash when driver was not loaded

--- pevCsrRegDev.c
+++ pevCsrRegDev.c
@@ -113,11 +113,12 @@ static regDevSupport pevCsrRegDevSupport = {
 static void pevCsrRegDevRegistrar ()
 {
     pevx_init(0);
-    if (pevx_board_name(0)[0])
-        regDevRegisterDevice("pev_csr", &pevCsrRegDevSupport,
-            NULL,  /* no need to pass any driver private data: card number is always 0 */
-            8*1024 /* is there any way to ask the PEV API for the size? 8k is the size of PCI bar 3 */
-        );
+    if (!pevx_get_driver_version()[0]) return;
+    if (!pevx_board_name(0)[0]) return;
+    regDevRegisterDevice("pev_csr", &pevCsrRegDevSupport,
+        NULL,  /* no need to pass any driver private data: card number is always 0 */
+        8*1024 /* is there any way to ask the PEV API for the size? 8k is the size of PCI bar 3 */
+    );
 }
 
 epicsExportRegistrar(pevCsrRegDevRegistrar);
